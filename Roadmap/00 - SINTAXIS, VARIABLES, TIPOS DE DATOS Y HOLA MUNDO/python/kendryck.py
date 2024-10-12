279b450b4976d1d09be54dab007489c8781da5ea
kendryck1 <kendryckquintero1@gmail.com>
2024-10-11 22:17:22 -0500

    Create kendryck.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/kendryck.py
@@ -0,0 +1,38 @@
+#Comentario en mi codigo y https://www.python.org/
+
+#Primera forma de hacer un comentario 
+
+'''
+Tambien con esto
+se puede hacer un 
+comentario
+'''
+
+
+"""
+Otra forma 
+de hacer 
+un comentario
+"""
+
+#Crea una variable (y una constante si el lenguaje lo soporta).
+ #Crea variables representando todos los tipos de datos primitivos
+
+variable = "kendryck"
+
+#constante, en este lenguaje de programacion no existe una sintaxis dedicada para la declaracion de constantes
+
+#Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+
+texto = "cadena de texto" # cadena de texto
+
+entero = 12 # int
+
+flotante = 8.5 # float
+
+booleano = True # booleano
+
+
+#Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+print("¡Hola, Python!")
